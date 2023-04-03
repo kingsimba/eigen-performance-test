15efbaad6f54b91c2f4886a1596c43c2c1573f3f
FengZhaolin <kingsimba0511@gmail.com>
2023-04-03 14:06:10 +0800

    add run_test.py

--- README.md
+++ README.md
@@ -4,16 +4,24 @@
 
 Compare eigen with traditional way of calculating large amount of standard deviation
 
-```
-Result (Raspberry PI model 4B - Compiled with GCC 8.3 with -O3):
-Eigen: 1071 ms
-Traditional: 2464 ms
-
-Result (AMD Ryzen 5 4600G - Compiled with clang 13.0.0 with -O3):
-Eigen: 129 ms
-Traditional: 777 ms
-
-Result (AMD Ryzen 5 4600G - Compiled with clang 13.0.0 without -O3):
-Eigen: 8750 ms (without optimization, Eigen is unbearably slow)
-Traditional: 3201 ms
-```
+| Test                       | Ryzen 4600G |
+| -------------------------- | ----------- |
+| calcEigenCasualFloat       | 0.919s      |
+| calcEigenCasualDouble      | 1.767s      |
+| calcEigenSinFloat          | 1.553s      |
+| calcTranditionalSinFloat   | 2.383s      |
+| calcEigenSinDouble         | 6.097s      |
+| calcTranditionalSinDouble  | 6.153s      |
+| calcEigenSqrtFloat         | 0.317s      |
+| calcTranditionalSqrtFloat  | 1.323s      |
+| calcEigenSqrtDouble        | 1.035s      |
+| calcTranditionalSqrtDouble | 2.14s       |
+| **PerformanceTest**        | **23.688s** |
+| cmpLessFloatFloat          | 0.952s      |
+| cmpLessFloatDouble         | 2.076s      |
+| cmpLessDoubleDouble        | 2.091s      |
+| cmpLessDoubleFloat         | 2.005s      |
+| **ComparisonTest**         | **7.124s**  |
+| eigen                      | 0.132s      |
+| traditional                | 0.817s      |
+| **ZeroDetectTest**         | **0.949s**  |
---
+++ run_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import json
+
+
+def find_shortest(names):
+    rtn = 0
+    for name in names:
+        if len(name) > rtn:
+            rtn = len(name)
+    return rtn
+
+
+def main():
+    if not os.path.isdir("build"):
+        os.mkdir("build")
+
+    # build
+    print("Building...")
+    subprocess.check_output("cd build && cmake .. && make", shell=True)
+
+    # run test
+    print("Running test...")
+    if not os.path.isfile("test_detail.json") or os.path.getctime(
+        "test_detail.json"
+    ) < os.path.getctime("build/performance_test"):
+        print("Test result is too old. Rerunning ...")
+        subprocess.check_output(
+            "build/performance_test --gtest_output=json", shell=True
+        )
+
+    # format output as Markdown
+    print("Formatting output...")
+    names = []
+    times = []
+    with open("test_detail.json", "r", encoding="utf8") as f:
+        root = json.load(f)
+
+    suites = root["testsuites"]
+    for suite in suites:
+        names.append(suite["name"] + "*" * 4)
+        times.append(suite["time"] + "*" * 4)
+        for s in suite["testsuite"]:
+            names.append(s["name"])
+            times.append(s["time"])
+
+    name_len = find_shortest(names)
+    time_len = find_shortest(times)
+
+    print(f"| {'Test':>{name_len}} | {'Time':>{time_len}} |")
+    print(f"| {'-'*name_len} | {'-'*time_len} |")
+
+    for suite in suites:
+        for s in suite["testsuite"]:
+            name = s["name"]
+            time = s["time"]
+            print(f"| {name:<{name_len}} | {time:>{time_len}} |")
+
+        name = "**" + suite["name"] + "**"
+        time = "**" + suite["time"] + "**"
+        print(f"| {name:<{name_len}} | {time:>{time_len}} |")
+
+
+if __name__ == "__main__":
+    main()
